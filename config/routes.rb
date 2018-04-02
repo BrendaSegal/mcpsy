5b1637e4ea307e2c1b949a73303d170fc8128b47
Tyler <alsburyt@gmail.com>
2018-04-02 13:37:42 -0400

    Homepage Route and base layout

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   mount Fae::Engine => '/admin'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'frontend/home#index'
 end
